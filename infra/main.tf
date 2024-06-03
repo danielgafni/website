7cfdf66776e4a9b514802e320bc7ce7278db2de5
Daniel Gafni <danielgafni16@gmail.com>
2024-06-03 19:46:45 +0300

    add ssh key

--- infra/main.tf
+++ infra/main.tf
@@ -36,6 +36,11 @@ resource "hcloud_ssh_key" "yubikey" {
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCowWxLVGvk6F1J/2vXcnJXpBu/9DSkAJ9gGX1vsDhKvVUoQZg41ABzWhisB/jmAiu4002FF1ed+UckJJ7F4FqrAMbXtw3BeyKpHH8Rl9WfDC4whFsg4MwsgPHSKg6AvSKrZh0tAY0dj4Pval/zzR5/FQ0ljkG+yBGr40RGLMXGbfu0xWHlANdhN+yGaGYaXtWZPxCW+PGwdgtXn2JKAzYjsJR6PCCdTki3/1+Ovr+I1mt6PT4qwgBV5VIWc9kwwAT5GQdifZHiCfxT6FCoPbtlqZv8h7TNEbGy3Aft3j5Aj2hRkd5eZ6ZhDIceoaLQwJzW5MKVC555+MbyuLzJ52c4daBgcH5mcn/SEuSB80HPmJdSg9D8bffC0XtO80EwOOZBpAhpblClId7CXRTEcJnssJdIVNFODLDd/p1qyK/jPqZzSdkLjpfmGuX65YIAmgpDjWe8BxZbCIjLspe5QVnjpeNCDK7DW5sTa9zwyYJ/0ghUSrHz79BC8tH62i7AOAfqB0N2gjv4ytKQ0eafbZZZFa9Ci7LqH0vj0xKsWP0PfyOpdtvVjfEBvz+Gk5w0VEfYuMlNLRkHHkLzbEO0R201R0M7eyypIWNFs3xXO5xXQOJtFA7/C8zNDNPpogiBT/J8VZtdDeB2Rr+3GSvaG8jpCJs572Q5j6IVUlho7hlk+w== openpgp:0xB0A185EE"
 }
 
+resource "hcloud_ssh_key" "danielgafni-website-github-actions" {
+  name = "danielgafni-website-github-actions"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCpITQKlC+lWz/famrrak90ElVzZV0pBn/WvtxojP5T5rNtfknlI1e1XI4aRrQCv7BAxS10vovDwTROt5ouVyS5pwC3BZLmKD5hPetEGKA4cXhbDmh9WDdK8dRoUqE0DCBZRi/zRBX1lqJMSUnqbPkdZv27uLV6NAbbIbytumbNanlNa3qmCy8ekMQTI9irYMXu6V1xng1Uqh16p6o/FofoUCDXwAsK2jxBs3QF5y/tSO9hNvFsTB4C9lAiCgXMIBQBZ71T2W02A1l713VQC+5nsfMM4UNSFpMffjeGsX/whism20tKZIVgm5RV7yjp7MVkb09RliNegmQeR2lsP20uzqUSraFl7yFB1qDF/rdZREMgXtIT3Yt3h0GCN/W/LLonUph0dO0B48vtM90gQafkG3fCjTXtYYDHNdFrh3CD+G4t/16efxSMQNvsdBpK0B5laQoHps5XGZLOkUhfzql0GrTLoHhlz19hCHXTsROstUOJjeOagT3iHvpKgu573JMIc1SRXWI0d6egeI6lsX7h4eGy6P9utwo5DaI0gVptN3LPwPRS/KP3gb6i1tvzkFLTlMpKPEyL7q9IO5JeKO6fRbVuHNSRjMvsGwS8xFWCmbqa8JPaPq3K4+NkxMluw1vRzsrkXW6eZYr1mnGTU/Tbc8HiJ+0fwRj8FdOfkshBSw== danielgafni16@gmail.com"
+}
+
 resource "hcloud_firewall" "website_firewall" {
   name = "website-firewall"
   rule {
@@ -88,7 +93,10 @@ resource "hcloud_server" "website" {
   image        = "ubuntu-22.04" # After provisioning, NixOS will be installed see @install
   server_type  = "cpx11"        # AMD 2 vCPU, 2 GB RAM, 40 GB NVMe SSD
   location     = "fsn1"
-  ssh_keys     = [hcloud_ssh_key.yubikey.id] # SSH keys associated with the server
+  ssh_keys     = [ # SSH keys associated with the server
+    hcloud_ssh_key.yubikey.id,
+    hcloud_ssh_key.danielgafni-website-github-actions.id
+    ]
   firewall_ids = [hcloud_firewall.website_firewall.id]
 }
 
